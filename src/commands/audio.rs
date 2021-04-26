53e57940ea27b3d5b8bc61840031946a270a785b
tony du <tony56a@gmail.com>
2021-04-25 23:38:41 -0400

    add clip function

--- src/commands/audio.rs
+++ src/commands/audio.rs
@@ -1,4 +1,3 @@
-use rand::seq::SliceRandom;
 use serenity::framework::standard::{
     macros::{command, group},
     Args, CommandResult,
@@ -6,6 +5,7 @@ use serenity::framework::standard::{
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 use serenity::utils::MessageBuilder;
+use songbird::tracks::PlayMode;
 use std::collections::HashMap;
 
 #[command]
@@ -36,10 +36,7 @@ pub async fn join(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
         })
         .collect();
 
-    //let voice_channels: HashMap<String, ChannelId>  = voice_channels_vec.into_iter().collect();
-
     if !voice_channels.contains_key(&voice_channel.to_lowercase()) {
-        println!("{:?}", voice_channels);
         msg.channel_id
             .say(
                 &ctx.http,
@@ -51,7 +48,10 @@ pub async fn join(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
     let manager = songbird::get(ctx).await.expect("Songbird client").clone();
 
     let _ = manager
-        .join(guild_id, u64::from(voice_channels[&voice_channel]))
+        .join(
+            guild_id,
+            u64::from(voice_channels[&voice_channel.to_lowercase()]),
+        )
         .await;
 
     Ok(())
@@ -75,8 +75,6 @@ pub async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
                 .say(&ctx.http, format!("Failed: {:?}", e))
                 .await;
         }
-
-        msg.channel_id.say(&ctx.http, "Left voice channel").await;
     } else {
         msg.reply(ctx, "Not in a voice channel").await;
     }
@@ -87,6 +85,9 @@ pub async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
 #[command]
 #[only_in(guilds)]
 async fn play(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let guild = msg.guild(&ctx.cache).await.unwrap();
+    let guild_id = guild.id;
+
     let mapping: HashMap<&str, &str> = [
         (
             "that was easy",
@@ -153,9 +154,7 @@ async fn play(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
             }
         };
 
-        handler.play_source(source);
-
-        msg.channel_id.say(&ctx.http, "Playing song").await;
+        handler.play_only_source(source);
     } else {
         msg.channel_id
             .say(&ctx.http, "Not in a voice channel to play in")
@@ -165,8 +164,126 @@ async fn play(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     Ok(())
 }
 
+#[command]
+#[only_in(guilds)]
+async fn clip(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let guild = msg.guild(&ctx.cache).await.unwrap();
+    let guild_id = guild.id;
+
+    let mapping: HashMap<&str, &str> = [
+        (
+            "that was easy",
+            "https://www.youtube.com/watch?v=OE_d8UCiXcI",
+        ),
+        ("yeet", "https://www.youtube.com/watch?v=EwlM3kpqEo0"),
+        ("bruh", "https://www.youtube.com/watch?v=2ZIpFytCSVc"),
+        ("fart", "https://www.youtube.com/watch?v=dEOjOkHSShM"),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    if args.is_empty() {
+        let response = mapping
+            .keys()
+            .cloned()
+            .into_iter()
+            .fold(
+                MessageBuilder::new()
+                    .push("Use me with \"the clip name\" \"the channel\"! Valid Clips are:\n "),
+                |cb, clip_name| cb.push(format!("\t* {}\n", clip_name)),
+            )
+            .build();
+        msg.channel_id.say(&ctx.http, response).await?;
+        return Ok(());
+    }
+
+    let clip_name = match args.single_quoted::<String>() {
+        Ok(name) => name.to_lowercase(),
+        Err(_) => {
+            msg.channel_id
+                .say(&ctx.http, "Clip name is not valid!")
+                .await;
+            return Ok(());
+        }
+    };
+    let voice_channel = args.single_quoted::<String>()?;
+
+    if !mapping.contains_key(&*clip_name) {
+        msg.channel_id
+            .say(&ctx.http, String::from("Effects are not available"))
+            .await?;
+        return Ok(());
+    }
+
+    let voice_channels: HashMap<String, ChannelId> = guild
+        .channels
+        .values()
+        .into_iter()
+        .filter_map(|channel| match channel.kind {
+            ChannelType::Voice => Some(((&channel).name.to_lowercase(), channel.id)),
+            _ => None,
+        })
+        .collect();
+
+    if !voice_channels.contains_key(&voice_channel.to_lowercase()) {
+        msg.channel_id
+            .say(
+                &ctx.http,
+                format!("{channel} doesn't exist!", channel = voice_channel),
+            )
+            .await?;
+        return Ok(());
+    }
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    let _ = manager
+        .join(
+            guild_id,
+            u64::from(voice_channels[&voice_channel.to_lowercase()]),
+        )
+        .await;
+
+    if let Some(handler_lock) = manager.get(guild_id) {
+        let mut handler = handler_lock.lock().await;
+        let source = match songbird::ytdl(mapping[&*clip_name]).await {
+            Ok(source) => source,
+            Err(why) => {
+                println!("Err starting source: {:?}", why);
+
+                msg.channel_id.say(&ctx.http, "Error sourcing ffmpeg").await;
+
+                return Ok(());
+            }
+        };
+
+        let handle = handler.play_only_source(source);
+        while handle.get_info().await?.playing == PlayMode::Play {}
+    } else {
+        msg.channel_id
+            .say(&ctx.http, "Not in a voice channel to play in")
+            .await;
+    }
+
+    let has_handler = manager.get(guild_id).is_some();
+    if has_handler {
+        if let Err(e) = manager.remove(guild_id).await {
+            msg.channel_id
+                .say(&ctx.http, format!("Failed: {:?}", e))
+                .await;
+        }
+    } else {
+        msg.reply(ctx, "Not in a voice channel").await;
+    }
+
+    Ok(())
+}
+
 #[group]
 #[prefix = "audio"]
 #[description = "Commands to audio"]
-#[commands(join, leave, play)]
+#[commands(join, leave, play, clip)]
 struct Audio;
