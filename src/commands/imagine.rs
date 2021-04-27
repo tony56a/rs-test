d04a3504d97fc1b10d734001e6175a5440b3b331
tony du <tony56a@gmail.com>
2021-04-26 23:31:42 -0400

    add deep dream command

--- src/commands/imagine.rs
+++ src/commands/imagine.rs
@@ -55,8 +55,40 @@ pub async fn picture(ctx: &Context, msg: &Message, mut args: Args) -> CommandRes
     Ok(())
 }
 
+#[command]
+pub async fn weird_shit(ctx: &Context, msg: &Message) -> CommandResult {
+
+    let res = {
+        let data_read = ctx.data.read().await;
+        let bot_config_lock = data_read
+            .get::<BotConfig>()
+            .expect("Expected BotConfig in TypeMap.")
+            .clone();
+        let bot_config = bot_config_lock.read().await;
+        let client = reqwest::Client::new();
+
+        let params = [("image", "https://loremflickr.com/640/480/animal")];
+
+        client
+            .post("https://api.deepai.org/api/deepdream")
+            .header(
+                "api-key",
+                bot_config.get::<str>(&constants::DEEPAI_TOKEN_KEY).unwrap(),
+            )
+            .form(&params)
+            .send()
+            .await?
+            .json::<ApiResponse>()
+            .await?
+    };
+
+    msg.channel_id.say(&ctx.http, res.output_url).await?;
+
+    Ok(())
+}
+
 #[group]
 #[prefix = "imagine"]
 #[description = "Commands to synthesize things via ML"]
-#[commands(picture)]
+#[commands(picture, weird_shit)]
 struct Imagine;
